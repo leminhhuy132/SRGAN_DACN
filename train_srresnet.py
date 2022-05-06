47a09948ea8faf4e4ce2890081f596d256a712b7
leminhhuy132 <minhhhuyle123@gmail.com>
2022-05-06 09:11:28 +0700

    fix dataloader

--- train_srresnet.py
+++ train_srresnet.py
@@ -142,16 +142,16 @@ def load_dataset() -> [CUDAPrefetcher, CUDAPrefetcher, CUDAPrefetcher]:
                                   persistent_workers=True)  # True will improve performances when you call into the dataloader
                                                             # multiple times in a row (as creating the workers is expensive).
     valid_dataloader = DataLoader(valid_datasets,
-                                  batch_size=config.batch_size,
+                                  batch_size=1,
                                   shuffle=False,
-                                  num_workers=config.num_workers,
+                                  num_workers=1,
                                   pin_memory=True,
                                   drop_last=False,
                                   persistent_workers=True)
     test_dataloader = DataLoader(test_datasets,
-                                 batch_size=config.batch_size,
+                                 batch_size=1,
                                  shuffle=False,
-                                 num_workers=config.num_workers,
+                                 num_workers=1,
                                  pin_memory=True,
                                  drop_last=False,
                                  persistent_workers=True)
